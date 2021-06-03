390f26cd744b6a9d0575e2e5850c3953f384ce65
mdinesh124 <79132253+mdinesh124@users.noreply.github.com>
2021-06-04 01:48:15 +0530

    Add files via upload

---
+++ Day6/Pattern.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Pattern {
+
+    public static String findPattern(String s, String p) {
+        if(s.contains(p))
+        {
+            return p.Inde
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String p = sc.nextLine();
+        System.out.println(findPattern(s,p));
+    }
+    
+}
