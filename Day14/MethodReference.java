7aefc87cabfc93318f18c73521b18413567be5c8
mdinesh124 <79132253+mdinesh124@users.noreply.github.com>
2021-06-12 01:11:35 +0530

    Create MethodReference.java

---
+++ Day14/MethodReference.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.util.function.*;
+
+class GFG {
+   
+	public static void main (String[] args) {
+	
+		List<Integer> al= new ArrayList<>(Arrays.asList(10,20,15,16));
+		
+		al.forEach(System.out::println);
+	}
+}
