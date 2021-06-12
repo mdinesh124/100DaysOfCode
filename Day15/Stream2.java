133fb23c5a937a23b984e5c16e5a2483d5b3dea0
mdinesh124 <79132253+mdinesh124@users.noreply.github.com>
2021-06-13 01:26:02 +0530

    Add files via upload

---
+++ Day15/Stream2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.util.function.*;
+
+
+
+
+class Stream2 {
+  
+	public static void main (String[] args) {
+	
+	 int arr[]={30,20,10};
+	 
+	 int sum=Arrays.stream(arr).sum();
+	 
+	 System.out.println(sum);
+	 
+
+	}
+	
+}
