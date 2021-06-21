a08a14821e19529eb5c7b88961f56471888099ad
mdinesh124 <79132253+mdinesh124@users.noreply.github.com>
2021-06-21 23:54:08 +0530

    Add files via upload

---
+++ Day24/IsSorted.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+class IsSorted
+{ 
+    static boolean Sorted(int n,int arr[])
+    {
+    	for(int i = 1; i < n; i++)
+    	{
+    	    if(arr[i] < arr[i - 1])
+    	        return false;
+    	}
+
+    	return true;
+    } 
+
+    public static void main(String args[]) 
+    { 
+       
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+       System.out.println(Sorted(n,arr));
+    } 
+}
