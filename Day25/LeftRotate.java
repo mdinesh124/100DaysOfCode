8c891d46c8a68619ea8fb2a747a5ab1dba87011b
mdinesh124 <79132253+mdinesh124@users.noreply.github.com>
2021-06-23 01:48:08 +0530

    Add files via upload

---
+++ Day25/LeftRotate.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class LeftRotate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            a[i] = sc.nextInt();
+        }
+        int temp = a[0];
+        for(int i = 1; i < n; i++)
+        {
+            a[i-1] = a[i];
+            
+        }
+        a[n-1] = temp;
+        for(int i = 0; i < n; i++)
+        {
+            System.out.println(a[i]);
+        }
+    }
+}
