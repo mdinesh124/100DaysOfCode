539dc47f9e1099aa669ecffd932886a52b899524
mdinesh124 <79132253+mdinesh124@users.noreply.github.com>
2021-07-13 01:50:00 +0530

    Create SelectionSort.java

---
+++ Day45/SelectionSort.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+public class SelectionSort{
+    public static void sort(int a[],int n)
+    {
+        for(int i=0;i<n-1;i++)
+        {
+            int min = i;
+            for(int j = i+1;j<n;j++)
+            {
+                if(a[j]<a[min])
+                {
+                    min = j;
+                }
+                int temp = a[min];
+                a[min] = a[i];
+                a[i] = temp;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(a[i]+"");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = sc.nextInt();
+        }
+        sort(a,n);
+    }
+}
