c67f828543fde759b323b752cdfb78f0b2d786f2
mdinesh124 <79132253+mdinesh124@users.noreply.github.com>
2021-07-17 01:23:16 +0530

    Create LomutoPartition.java

---
+++ Day49/LomutoPartition.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class LomutoPartition
+{
+    public static void main (String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        iPartition(arr,0,n-1);
+        
+	    for(int x: arr)
+	        System.out.print(x+" ");
+        
+    }
+    
+    public static int iPartition(int arr[], int l, int h)
+    {   
+        int pivot=arr[h];
+        int i=l-1;
+        for(int j=l;j<=h-1;j++){
+            if(arr[j]<pivot){
+                i++;
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        int temp=arr[i+1];
+        arr[i+1]=arr[h];
+        arr[h]=temp;
+        return i+1;
+    }
+}
