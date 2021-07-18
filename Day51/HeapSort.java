c76fedeac9318e012a46e126850c722cd91c3a9f
mdinesh124 <79132253+mdinesh124@users.noreply.github.com>
2021-07-19 01:51:25 +0530

    Create HeapSort.java

---
+++ Day51/HeapSort.java
@@ -0,0 +1,72 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+  
+public class HeapSort 
+{ 
+	public void buildheap(int arr[],int n){
+        for (int i = n / 2 - 1; i >= 0; i--) 
+		    heapify(arr, n, i);
+    }
+	
+	public void sort(int arr[]) 
+	{ 
+		int n = arr.length; 
+
+		buildheap(arr,n); 
+ 
+		for (int i=n-1; i>0; i--) 
+		{ 
+			 
+			int temp = arr[0]; 
+			arr[0] = arr[i]; 
+			arr[i] = temp; 
+
+			heapify(arr, i, 0); 
+		} 
+	} 
+
+	void heapify(int arr[], int n, int i) 
+	{ 
+		int largest = i;  
+		int l = 2*i + 1; 
+		int r = 2*i + 2; 
+
+		if (l < n && arr[l] > arr[largest]) 
+			largest = l; 
+ 
+		if (r < n && arr[r] > arr[largest]) 
+			largest = r; 
+
+		if (largest != i) 
+		{ 
+			int swap = arr[i]; 
+			arr[i] = arr[largest]; 
+			arr[largest] = swap; 
+
+			heapify(arr, n, largest); 
+		} 
+	} 
+
+	static void printArray(int arr[]) 
+	{ 
+		int n = arr.length; 
+		for (int i=0; i<n; ++i) 
+			System.out.print(arr[i]+" "); 
+		System.out.println(); 
+	} 
+ 
+	public static void main(String args[]) 
+	{ 
+		Scanner sc = new Scanner(System.in); 
+        int n = sc.nextInt();
+        int arr[] = new int[n]; 
+        for(int i=0; i<n; i++) 
+        { 
+            arr[i] = sc.nextInt();
+        }
+		HeapSort ob = new HeapSort(); 
+		ob.sort(arr);
+		printArray(arr); 
+	} 
+} 
