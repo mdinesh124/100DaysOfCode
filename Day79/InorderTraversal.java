f63d124808a966ffbeb65a474967b793c9cf447e
mdinesh124 <79132253+mdinesh124@users.noreply.github.com>
2021-08-16 00:58:11 +0530

    Create InorderTraversal.java

---
+++ Day79/InorderTraversal.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+class Node  
+{ 
+  int key; 
+  Node left; 
+  Node right; 
+  Node(int k){
+      key=k;
+      left=right=null;
+  }
+}
+
+
+class InorderTraversal { 
+    
+    public static void main(String args[]) 
+    { 
+        Node root=new Node(10);
+    	root.left=new Node(20);
+    	root.right=new Node(3);
+    	root.right.left=new Node(4);
+    	root.right.right=new Node(50);
+    	
+    	inorder(root);
+    } 
+    
+    public static void inorder(Node root){
+        if(root!=null){
+            inorder(root.left);
+            System.out.print(root.key+" ");
+            inorder(root.right);    
+        }
+    } 
+} 
