ebee39feee4884928cb51a2e6c53fd397e164ad8
mdinesh124 <79132253+mdinesh124@users.noreply.github.com>
2021-08-23 00:57:56 +0530

    Create BinaryHeap.java

---
+++ Day86/BinaryHeap.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+  
+class BinaryHeap { 
+    
+    public static class MinHeap{
+        int arr[];
+        int size;
+        int capacity;
+        
+        MinHeap(int c){
+        size = 0; 
+        capacity = c; 
+        arr = new int[c];
+        }
+    
+        int left(int i) { return (2*i + 1); } 
+        int right(int i) { return (2*i + 2); } 
+        int parent(int i) { return (i-1)/2; } 
+    }
+    
+    public static void main(String args[]) 
+    { 
+        MinHeap h=new MinHeap(11);
+    	
+    } 
+   
+} 
