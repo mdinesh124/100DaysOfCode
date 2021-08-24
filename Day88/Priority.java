8df4a20c3b95af8cf2673bfcf96dddc6975a717a
mdinesh124 <79132253+mdinesh124@users.noreply.github.com>
2021-08-25 01:36:52 +0530

    Update Priority.java

--- Day88/Priority.java
+++ Day88/Priority.java
@@ -1,3 +1,5 @@
+/*MIN HEAP*/
+
 import java.util.*;
 import java.io.*;
 public class Priority {
@@ -11,3 +13,24 @@ public class Priority {
         System.out.println(queue.peek());
     }
 }
+
+
+
+
+ /*MAX HEAP*/
+
+/*import java.util.*;
+import java.io.*;
+public class Priority {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(Collections.reverseOrder());
+        queue.add(5);
+        queue.add(10);
+        queue.add(20);
+        System.out.println(queue.peek());
+        System.out.println(queue.poll());
+        System.out.println(queue.peek());
+    }
+}*/
+
+
